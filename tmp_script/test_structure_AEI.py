6423b4a59c18230a2399829423e6398ed2947c5c
taotao <335702517@qq.com>
2020-01-16 14:11:07 +0800

    update

--- tmp_script/test_structure_AEI.py
+++ tmp_script/test_structure_AEI.py
@@ -1,15 +1,19 @@
 import sys
 sys.path.append('..')
 from network.AEI_Net import *
+import torch.optim as opt
 import time
+from apex import amp
 
 device = torch.device('cuda:0')
 print(torch.backends.cudnn.benchmark)
-# torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark = True
 
-net = (AEI_Net(256).cuda())
+net = AEI_Net(256).cuda()
+optm = opt.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
+net, optm = amp.initialize(net, optm, opt_level="O3")
 
-batch_size = 4
+batch_size = 2
 
 z_id = torch.ones([batch_size, 256]).to(device)
 x = torch.zeros([batch_size, 3, 256, 256]).to(device)
@@ -17,6 +21,8 @@ x = torch.zeros([batch_size, 3, 256, 256]).to(device)
 while True:
     st = time.time()
     y = net(x, z_id)
-    y.mean().backward()
+    loss = y.mean()
+    with amp.scale_loss(loss, optm) as scaled_loss:
+        scaled_loss.backward()
     st = time.time() - st
     print(f'{st} sec')
