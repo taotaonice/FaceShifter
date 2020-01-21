980a6d1906aff5acb4cbb8e609abd0a7345f968f
taotao <335702517@qq.com>
2020-01-21 15:40:15 +0800

    update

--- face_modules/preprocess_images.py
+++ face_modules/preprocess_images.py
@@ -12,7 +12,7 @@ import pickle
 
 img_root_dir = '/home/taotao/Downloads/celeba-512/'
 save_path = '/home/taotao/Downloads/celeb-aligned-256/'
-embed_path = '/home/taotao/Downloads/celeb-aligned-256/embed.pkl'
+# embed_path = '/home/taotao/Downloads/celeb-aligned-256/embed.pkl'
 
 device = torch.device('cuda:0')
 mtcnn = MTCNN()
@@ -41,19 +41,19 @@ for root, dirs, files in os.walk(img_root_dir):
                 face = mtcnn.align(Image.fromarray(img[:, :, ::-1]), crop_size=(256, 256))
                 if face is None:
                     continue
-                scaled_img = face.resize((112, 112), Image.ANTIALIAS)
-                with torch.no_grad():
-                    embed = model(test_transform(scaled_img).unsqueeze(0).cuda()).squeeze().cpu().numpy()
+                # scaled_img = face.resize((112, 112), Image.ANTIALIAS)
+                # with torch.no_grad():
+                #     embed = model(test_transform(scaled_img).unsqueeze(0).cuda()).squeeze().cpu().numpy()
                 new_path = '%08d.jpg'%ind
                 ind += 1
                 print(new_path)
                 face.save(os.path.join(save_path, new_path))
-                embed_map[new_path] = embed.detach().cpu()
+                # embed_map[new_path] = embed.detach().cpu()
             except Exception as e:
                 continue
 
-with open(embed_path, 'wb') as f:
-    pickle.dump(embed_map, f)
+# with open(embed_path, 'wb') as f:
+#     pickle.dump(embed_map, f)
 #
 # img = cv2.imread('/home/taotao/Pictures/47d947b4d9cf3e2f62c0c8023a1c0dea.jpg')[:,:,::-1]
 # # bboxes, faces = mtcnn.align_multi(Image.fromarray(img), limit=10, min_face_size=30)
